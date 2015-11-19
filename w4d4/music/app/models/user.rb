49ad1f05b69720152329f3b61744b91ecd8caccd
Winnie Ngo <ngowinn@gmail.com>
2015-11-19 11:31:19 -0800

    add user authentication methods

---
+++ w4d4/music/app/models/user.rb
@@ -0,0 +1,41 @@
+class User < ActiveRecord::Base
+
+	attr_reader :password
+
+	validates :email, :password_digest, :session_token, presence: true
+	validates :email, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+	after_initialize :ensure_session_token
+
+	def self.generate_session_token
+		SecureRandom::urlsafe_base64(16)
+	end
+
+	def reset_session_token!
+		self.session_token = self.class.generate_session_token
+		self.save!
+		self.session_token
+	end
+
+	def password=(password)
+		self.password_digest = BCrypt::Password.create(password).to_s
+	end
+
+	def is_password?(password)
+		BCrypt::Password.new(self.password_digest).is_password?(password)
+	end
+
+	def find_by_credentials(email, password)
+		user = User.find_by(email: email)
+		return nil if user.nil?
+		user.is_password(password) ? user : nil
+	end
+
+
+	private
+
+	def ensure_session_token
+		self.session_token ||= self.class.generate_session_token
+	end
+end
---
+++ w4d4/music/db/seeds.rb
@@ -0,0 +1,2 @@
+User.create!(email: 'wngo@gmail.com', password: 'winniengo')
+User.create!(email: 'deager@gmail.com', password: 'danieleager')
