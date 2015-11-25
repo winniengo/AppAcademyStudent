0b4562ee28c9c223a5210c4a6e3b616416be769a
Winnie Ngo <ngowinn@gmail.com>
2015-11-24 17:52:28 -0800

    p05

--- w5d2/RailsLite/bin/p05_router_server.rb
+++ w5d2/RailsLite/bin/p05_router_server.rb
@@ -32,7 +32,7 @@ end
 
 router = Router.new
 router.draw do
-  get Regexp.new("^/cats$"), Cats2Controller, :index
+  get(Regexp.new("^/cats$"), Cats2Controller, :index)
   get Regexp.new("^/cats/(?<cat_id>\\d+)/statuses$"), StatusesController, :index
 end
 
--- w5d2/RailsLite/lib/controller_base.rb
+++ w5d2/RailsLite/lib/controller_base.rb
@@ -2,6 +2,8 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
 require 'byebug'
+require 'json'
+
 require_relative './session'
 
 class ControllerBase
@@ -11,7 +13,7 @@ class ControllerBase
   def initialize(req, res, route_params = {})
     @req = req
     @res = res
-    @params = route_params
+    @params = req.params.merge(route_params)
     @already_built_response = false
   end
 
@@ -25,6 +27,8 @@ class ControllerBase
     raise "attempting to render twice" if already_built_response?
     @res.status = 302
     @res['Location'] = url
+
+    @session.store_session(@res)
     @already_built_response = true
   end
 
@@ -35,6 +39,8 @@ class ControllerBase
     raise "attempting to render twice" if already_built_response?
     @res['Content-Type'] = content_type
     @res.write(content)
+    
+    @session.store_session(@res)
     @already_built_response = true
   end
 
@@ -42,17 +48,20 @@ class ControllerBase
   # pass the rendered html to render_content
   def render(template_name)
     controller_name = self.class.to_s.underscore 
-    path = "views/#{controller_name}/#{template_name}.html.erb"
-    content = ERB.new(File.read(path)).result(binding)
+    file_path = "views/#{controller_name}/#{template_name}.html.erb"
+    content = ERB.new(File.read(file_path)).result(binding)
     render_content(content, "text/html")
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    self.send(name)
+    render(name) unless @already_built_response
   end
 end
 
